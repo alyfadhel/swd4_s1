6700aea6057306b27792b2a898387d9362168a86
alyfa <alyfadel67@gmail.com>
2025-03-18 13:19:18 +0200

    Device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:swd4_s1/bmi_screen.dart';
 import 'package:swd4_s1/counter_screen.dart';
@@ -5,7 +7,12 @@ import 'package:swd4_s1/login_screen.dart';
 import 'package:swd4_s1/messenger_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
